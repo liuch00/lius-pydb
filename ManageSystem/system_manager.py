a2a8de6da28a1b98e7dfa0c845392ee03ca49f45
liuxinghan <liuxinghan99@163.com>
2022-01-06 16:34:02 +0800

    system

--- ManageSystem/system_manager.py
+++ ManageSystem/system_manager.py
@@ -1,23 +1,123 @@
 
-
+from pathlib import Path
+from antlr4 import InputStream, CommonTokenStream
+from antlr4.error.Errors import ParseCancellationException
+from antlr4.error.ErrorListener import ErrorListener
 from .system_visitor import SystemVisitor
 from FileSystem.FileManager import FileManager
 from FileSystem.BufManager import BufManager
+from RecordSystem.RecordManager import RecordManager
 from IndexSystem.index_manager import IndexManager
-from MetaSystem.MetaManager import MetaManager
+from MetaSystem.MetaHandler import MetaHandler
+from Exceptions.exception import *
+from .lookup_element import LookupOutput
+from SQL_Parser.SQLLexer import SQLLexer
+from SQL_Parser.SQLParser import SQLParser
+from MetaSystem.info import TableInfo, ColumnInfo
 
-from pathlib import Path
 
 class SystemManger:
-    def __init__(self, visitor: SystemVisitor, syspath: Path, bm: BufManager, im: IndexManager, mm: MetaManager):
+    def __init__(self, visitor: SystemVisitor, syspath: Path, bm: BufManager, rm: RecordManager, im: IndexManager):
         self.visitor = visitor
         self.systemPath = syspath
         self.BM = bm
         self.IM = im
-        self.MM = mm
+        self.RM = rm
+        self.metaHandlers = {}
         self.databaselist = []
-        for dbname in syspath.iterdir():
-            self.databaselist.append(dbname)
+        for dir in syspath.iterdir():
+            self.databaselist.append(dir.name)
         self.inUse = None
         self.visitor.manager = self
 
+    def shutdown(self):
+        self.IM.close_manager()
+        self.RM.shutdown()
+        self.BM.shutdown()
+
+    def createDatabase(self, dbname: str):
+        if dbname not in self.databaselist:
+            path: Path = self.systemPath / dbname
+            path.mkdir(parents=True)
+            self.databaselist.append(dbname)
+        else:
+            print("OH NO")
+            raise DatabaseAlreadyExist("this name exists")
+        return
+
+    def removeDatabase(self, dbname: str):
+        if dbname in self.databaselist:
+            self.IM.shut_handler(dbname)
+            if self.metaHandlers.get(dbname) is not None:
+                self.metaHandlers.pop(dbname).shutdown()
+            path: Path = self.systemPath / dbname
+            for table in path.iterdir():
+                if path.name.endswith(".table"):
+                    self.RM.closeFile(str(table))
+                table.unlink()
+            self.databaselist.remove(dbname)
+            path.rmdir()
+            if self.inUse == dbname:
+                self.inUse = None
+                result = LookupOutput(change_db='None')
+                return result
+        else:
+            print("OH NO")
+            raise DatabaseNotExist("this name doesn't exist")
+
+    def useDatabase(self, dbname: str):
+        if dbname in self.databaselist:
+            self.inUse = dbname
+            result = LookupOutput(change_db=dbname)
+            return result
+
+    def getTablePath(self, table: str):
+        if self.inUse is None:
+            print("OH NO")
+            raise NoDatabaseInUse("use a database first")
+        tablePath = self.systemPath / self.inUse / table
+        return str(tablePath) + ".table"
+
+    def execute(self, sql):
+        class StringErrorListener(ErrorListener):
+            def syntaxError(self, recognizer, offending_symbol, line, column, msg, e):
+                raise ParseCancellationException("line " + str(line) + ":" + str(column) + " " + msg)
+
+        self.visitor.time_cost()
+        input_stream = InputStream(sql)
+        lexer = SQLLexer(input_stream)
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(StringErrorListener())
+        tokens = CommonTokenStream(lexer)
+        parser = SQLParser(tokens)
+        parser.removeErrorListeners()
+        parser.addErrorListener(StringErrorListener())
+        try:
+            tree = parser.program()
+        except ParseCancellationException as e:
+            return [LookupOutput(None, None, str(e), cost=self.visitor.time_cost())]
+        try:
+            return self.visitor.visit(tree)
+        except MyException as e:
+            return [LookupOutput(message=str(e), cost=self.visitor.time_cost())]
+
+    def displayTableNames(self):
+        result = []
+        if self.inUse is not None:
+            usingDB = self.systemPath / self.inUse
+            for file in usingDB.iterdir():
+                if file.name.endswith(".table")
+                    result.append(file.stem)
+            return result
+        print("OH NO")
+        raise NoDatabaseInUse("use a database first")
+
+    def createTable(self, table: TableInfo):
+        if self.inUse is not None:
+            if self.metaHandlers.get(self.inUse) is None:
+                self.metaHandlers[self.inUse] = MetaHandler(self.inUse, str(self.systemPath))
+            metaHandler: MetaHandler = self.metaHandlers.get(self.inUse)
+            metaHandler.insertTable(table)
+
+        print("OH NO")
+        raise NoDatabaseInUse("use a database first")
