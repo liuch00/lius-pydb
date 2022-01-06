c5aeb35074371a60c321f5e3ade1c7cbd4187c7e
liuchenghua <liuch.russell@gmail.com>
2022-01-06 09:44:01 +0800

    finish some smellcode

--- IndexSystem/index_handler.py
+++ IndexSystem/index_handler.py
@@ -5,8 +5,8 @@ from ..FileSystem import macro
 
 
 class IndexHandler:
-    def __init__(self, file_manager: BufManager, database_name, home_directory):
-        self._manager = file_manager
+    def __init__(self, buf_manager: BufManager, database_name, home_directory):
+        self._manager = buf_manager
         index_file_name = database_name + macro.INDEX_NAME
         index_file_path = home_directory / database_name / index_file_name
         if not self._manager.fileExist(index_file_path):
