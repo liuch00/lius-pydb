27d6c0aad0be2b924d70dfcbe514b56174e7dca5
liuxinghan <liuxinghan99@163.com>
2022-01-06 15:15:09 +0800

    system

--- ManageSystem/system_manager.py
+++ ManageSystem/system_manager.py
@@ -1,3 +1,23 @@
+
+
+from .system_visitor import SystemVisitor
+from FileSystem.FileManager import FileManager
+from FileSystem.BufManager import BufManager
+from IndexSystem.index_manager import IndexManager
+from MetaSystem.MetaManager import MetaManager
+
+from pathlib import Path
+
 class SystemManger:
-    def __init__(self):
-        pass
+    def __init__(self, visitor: SystemVisitor, syspath: Path, bm: BufManager, im: IndexManager, mm: MetaManager):
+        self.visitor = visitor
+        self.systemPath = syspath
+        self.BM = bm
+        self.IM = im
+        self.MM = mm
+        self.databaselist = []
+        for dbname in syspath.iterdir():
+            self.databaselist.append(dbname)
+        self.inUse = None
+        self.visitor.manager = self
+
