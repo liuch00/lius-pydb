18ec8911893b5821cb197edf827883bfada6e6cc
liuchenghua <liuch.russell@gmail.com>
2022-01-06 15:14:16 +0800

    add lookup elements

---
+++ ManageSystem/lookup_element.py
@@ -0,0 +1,43 @@
+class Term:
+    """term_type:   0 is null
+                    1 is compare
+                    2 is in
+                    3 is like
+    """
+
+    def __init__(self, term_type, table_name, col, operator, aim_table_name, aim_col, value):
+        self._type: int = term_type
+        self._table: str = table_name
+        self._col: str = col
+        self._operator: str = operator
+        self._aim_table: str = aim_table_name
+        self._aim_col: str = aim_col
+        self._value = value
+
+
+class Reducer:
+    """reducer_type:0 is all
+                    1 is field
+                    2 is aggregation
+                    3 is counter
+    """
+
+    def __init__(self, reducer_type, table_name, col, aggregator):
+        self._reducer_type: int = reducer_type
+        self._table_name: str = table_name
+        self._col: str = col
+        self._aggregator: str = aggregator
+
+    def to_string(self, prefix=True):
+        # todo:
+        pass
+
+    def select(self, data: tuple):
+        # todo:
+        pass
+
+
+
+class Lookup_Output:
+    def __init__(self):
+        pass
--- ManageSystem/system_visitor.py
+++ ManageSystem/system_visitor.py
@@ -1,3 +1,6 @@
+from SQL_Parser import *
+from system_manager import SystemManger
+
 class SystemVisitor:
     def __init__(self):
         pass
